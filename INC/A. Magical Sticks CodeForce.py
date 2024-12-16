0f09565b688290975d74dbeb3742ce8001990a42
Arudhi <106733103+Arudohi@users.noreply.github.com>
2024-12-16 07:45:18 +0700

    Update A. Magical Sticks CodeForce.py
    add explanation in Indonesia Language

--- INC/A. Magical Sticks CodeForce.py
+++ INC/A. Magical Sticks CodeForce.py
@@ -1,11 +1,22 @@
-# Read number of test cases
-t = int(input())
+# Membaca jumlah test case
+t = int(input()) # => fungsi ini untuk membaca input jumlah test case (t) dan merepresentasikan jumlah test case.
 
-# Process each test case
-results = []
+# Memproses setiap test case
+results = [] # => dibuat untuk menyimpan output untuk setiap test case. 
 for _ in range(t):
     n = int(input())
-    results.append((n // 2) + (n % 2))
+    
+# Ini digunakkan untuk memulai perulangan yang berjalan selama t kali => (for _ in range(t):)
+# “_” adalah variabel penampung karena indeks perulangan tidak diperlukan. => (for _ in range(t):)
+# Pada setiap iterasi, program membaca sebuah bilangan bulat “n”, yang merupakan input untuk test case saat ini.
 
+    results.append((n // 2) + (n % 2))
+    # "(n % 2)” memeriksa apakah ada sisa (1 jika n ganjil, 0 jika genap).
+    # “+” Menambahkan dua nilai untuk menentukan jumlah kelompok terkecil dari ukuran 2 yang diperlukan.
+    # Menambahkan hasil ke daftar hasil.
+    
 # Print results
 print('\n'.join(map(str, results)))
+# Mengonversi setiap hasil dalam daftar hasil menjadi sebuah string menggunakan map(str, hasil).
+# Menggabungkan string dengan karakter baris baru \n, membuat satu string di mana setiap hasil muncul pada baris baru.
+# Mencetak string, menampilkan satu hasil per baris.
